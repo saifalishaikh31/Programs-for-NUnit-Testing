97cbcdbb55655f17ad1bbc3d06101fbb7a95f91e
saifalishaikh31 <saifalishaikh31>
2022-02-01 16:41:40 +0530

    [Saif Ali] Add: Vending Machine Program Added

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             bool flag = true;
+             while (flag)
+             {
+                 Console.WriteLine("\n 1.Vending Machine."
+                                  + "\n 2.Exit");
+                 int option = Convert.ToInt32(Console.ReadLine());
+                 switch (option)
+                 {
+                     case 1:
+                         VendingMachine vendingmachine = new VendingMachine();
+                         vendingmachine.Amount();
+                         break;
+                     case 2:
+                         flag = false;
+                         break;
+                     default: 
+                         Console.WriteLine("Choose Correct option");
+                         break;
+
+                 }
+             }
+        }
+    }
+}
---
+++ NUnitTesting/VendingMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class VendingMachine
+    {
+        public void Amount()
+        {
+            Console.WriteLine("Enter the amount to change :");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            vendingMachine(amount);
+        }
+        public static void vendingMachine(int amount)
+        {
+            int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] noteCounter = new int[notes.Length];
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                }
+            }
+
+            Console.WriteLine("Currency Count ->");
+            int noOfNotes = 0;
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i].ToString().PadLeft(4) + " : " + noteCounter[i]);
+                    noOfNotes += noteCounter[i];
+                }
+            }
+            Console.WriteLine("Minimum number of notes : " + noOfNotes);
+        }
+    }
+}
